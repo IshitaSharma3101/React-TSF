a082b30a520e1e166ff3f1427fcfd1806c2735f0
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-02-21 15:20:41 +0530

    initial commit

---
+++ src/components/Header/header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return(
+        <div>
+            <h1 className="web-name">Wagging Tails</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="donate.html">Donate</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Home/home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return(
+        <h3 className="dog">“Dogs do speak, but only to those who know how to listen.” <span style={{color:"#ffb37a"}}>– Orhan Pamuk</span></h3>
+    )
+}
+
+export default Home
